b985af378975cb828741975810f10acb29ff5465
Gary Rennie <webmaster@gazler.com>
2011-09-13 18:51:08 +0100

    updated consumer to use sessions

--- consumer/consumer.rb
+++ consumer/consumer.rb
@@ -1,9 +1,13 @@
 require 'sinatra'  
 require 'oauth2'  
+require 'json'
+enable :sessions
   
 def client  
   OAuth2::Client.new('6IFWs6UowWqbDa4u1LCbZBpXWs2c8WbnkloWd90u', 'CosAmQVJcXPCFX4nqynCH6p67Yv2znLkOT3KpsiO', :site => 'http://localhost:3000')  
 end  
+
+  
   
 get '/auth/test' do  
   redirect client.web_server.authorize_url(  
@@ -13,10 +17,25 @@ end
   
 get '/auth/test/callback' do  
   access_token = client.web_server.get_access_token(params[:code], :redirect_uri => redirect_uri)  
-  p access_token
-  response = access_token.get('/api/v1/data.json')
-  response.inspect
+  session[:access_token] = access_token.token
+  @message = JSON.parse(access_token.get('/api/v1/data.json'))
+  erb :success
 end  
+
+get '/another_page' do
+  @message = get_response('data.json')
+  erb :different
+end
+
+get '/a_different_page' do
+  @message = get_response('data.json')
+  erb :success
+end
+  
+def get_response(url)
+  access_token = OAuth2::AccessToken.new(client, session[:access_token])
+  JSON.parse(access_token.get("/api/v1/#{url}")) 
+end
   
 def redirect_uri  
   uri = URI.parse(request.url)  
@@ -24,3 +43,4 @@ def redirect_uri
   uri.query = nil
   uri.to_s
 end  
+
